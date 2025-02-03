eee5709fcb7cd96302120586dd200fdc96d0025f
diegomrno <diegomoreno.pro45@gmail.com>
2025-02-04 00:37:49 +0100

    feat: Better logs utility

--- ui/editor/app/instances/logs_utility/logs_utility.cpp
+++ ui/editor/app/instances/logs_utility/logs_utility.cpp
@@ -85,146 +85,154 @@ namespace VortexEditor
     {
         ImGui::PushStyleColor(ImGuiCol_ChildBg, ImVec4(0.1f, 0.1f, 0.1f, 0.0f));
 
-        if (ImGui::BeginChild("TerminalChild", ImVec2(0, 0), true, ImGuiWindowFlags_AlwaysVerticalScrollbar))
+        static ImGuiTableFlags flags = ImGuiTableFlags_Resizable | ImGuiTableFlags_Reorderable | ImGuiTableFlags_Hideable | ImGuiTableFlags_Sortable | ImGuiTableFlags_RowBg | ImGuiTableFlags_Borders | ImGuiTableFlags_ScrollY | ImGuiTableFlags_SizingStretchSame;
+
+        const float TEXT_BASE_WIDTH = ImGui::CalcTextSize("A").x;
+
+        if (ImGui::BeginTable("LogsTable", 4, flags))
         {
-            static ImGuiTableFlags flags = ImGuiTableFlags_Resizable | ImGuiTableFlags_Reorderable | ImGuiTableFlags_Hideable | ImGuiTableFlags_Sortable;
+            ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, ImVec2(10.0f, 6.0f));
 
-            const float TEXT_BASE_WIDTH = ImGui::CalcTextSize("A").x;
+            ImGui::TableSetupColumn("Level", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 6.0f);
+            ImGui::TableSetupColumn("Origin", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 9.0f);
+            ImGui::TableSetupColumn("Timestamp", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 15.0f);
+            ImGui::TableSetupColumn("Message", ImGuiTableColumnFlags_WidthStretch);
 
-            if (ImGui::BeginTable("LogsTable", 4, flags | ImGuiTableFlags_Borders))
+            ImGui::TableHeadersRow();
+
+            ImGui::PopStyleVar();
+            for (auto log : VortexMaker::GetCurrentContext()->registered_logs)
             {
-                ImGui::TableSetupColumn("Level", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 6.0f);
-                ImGui::TableSetupColumn("Origin", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 9.0f);
-                ImGui::TableSetupColumn("Timestamp", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 15.0f);
-                ImGui::TableSetupColumn("Message", ImGuiTableColumnFlags_WidthStretch);
+                ImGui::TableNextRow();
 
-                for (auto log : VortexMaker::GetCurrentContext()->registered_logs)
+                if ((log->m_level == spdlog::level::critical && !FatalFilter) ||
+                    (log->m_level == spdlog::level::err && !ErrorFilter) ||
+                    (log->m_level == spdlog::level::warn && !WarnFilter) ||
+                    (log->m_level == spdlog::level::info && !InfoFilter))
                 {
-                    if ((log->m_level == spdlog::level::critical && !FatalFilter) ||
-                        (log->m_level == spdlog::level::err && !ErrorFilter) ||
-                        (log->m_level == spdlog::level::warn && !WarnFilter) ||
-                        (log->m_level == spdlog::level::info && !InfoFilter))
-                    {
-                        continue;
-                    }
+                    continue;
+                }
 
-                    ImGui::TableNextRow();
+                bool is_hovered = ImGui::IsItemHovered();
+                if (is_hovered)
+                {
+                    ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+                }
 
-                    bool is_hovered = ImGui::IsItemHovered();
-                    if (is_hovered)
-                    {
-                        ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
-                    }
+                if (ImGui::IsMouseClicked(0) && is_hovered)
+                {
+                    std::string content_to_copy = log->m_timestamp + " | " + log->m_filter + " | " + log->m_message;
+                    ImGui::SetClipboardText(content_to_copy.c_str());
+                }
 
-                    if (ImGui::IsMouseClicked(0) && is_hovered)
+                for (int i = 0; i <= 3; i++)
+                {
+                    ImGui::TableSetColumnIndex(i);
+                    if (i == 0)
                     {
-                        std::string content_to_copy = log->m_timestamp + " | " + log->m_filter + " | " + log->m_message;
-                        ImGui::SetClipboardText(content_to_copy.c_str());
-                    }
-
-                    for (int i = 0; i <= 3; i++)
-                    {
-                        ImGui::TableSetColumnIndex(i);
-                        if (i == 0)
+                        ImVec4 color;
+                        switch (log->m_level) // Add "User input" in blue
                         {
-                            ImVec4 color;
-                            switch (log->m_level)
-                            {
-                            case spdlog::level::critical:
-                                color = ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
-                                break;
-                            case spdlog::level::err:
-                                color = ImVec4(0.8f, 0.2f, 0.2f, 1.0f);
-                                break;
-                            case spdlog::level::warn:
-                                color = ImVec4(0.8f, 0.8f, 0.0f, 1.0f);
-                                break;
-                            case spdlog::level::info:
-                                color = ImVec4(0.0f, 1.0f, 0.0f, 0.0f);
-                                break;
-                            default:
-                                color = ImVec4(0.5f, 0.5f, 0.5f, 1.0f);
-                                break;
-                            }
-
-                            ImVec2 pos = ImGui::GetCursorScreenPos();
-                            float width = ImGui::GetFontSize() * 0.4f;
-                            float height = ImGui::GetFontSize() * 1.2f;
-                            float rounding = 3.0f;
-                            ImVec2 rect_min = pos;
-                            ImVec2 rect_max = ImVec2(pos.x + width, pos.y + height);
-
-                            ImGui::GetWindowDrawList()->AddRectFilled(rect_min, rect_max, ImGui::GetColorU32(color), rounding);
-
-                            ImGui::SameLine();
-
-                            switch (log->m_level)
-                            {
-                            case spdlog::level::critical:
-                                ImGui::Text("FATAL");
-                                break;
-                            case spdlog::level::err:
-                                ImGui::Text("Error");
-                                break;
-                            case spdlog::level::warn:
-                                ImGui::Text("Warning");
-                                break;
-                            case spdlog::level::info:
-                                ImGui::Text("Info");
-                                break;
-                            default:
-                                ImGui::Text("");
-                                break;
-                            }
+                        case spdlog::level::critical:
+                            color = ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
+                            break;
+                        case spdlog::level::err:
+                            color = ImVec4(0.8f, 0.2f, 0.2f, 1.0f);
+                            break;
+                        case spdlog::level::warn:
+                            color = ImVec4(0.8f, 0.8f, 0.0f, 1.0f);
+                            break;
+                        case spdlog::level::info:
+                            color = ImVec4(0.0f, 1.0f, 0.0f, 0.0f);
+                            break;
+                        default:
+                            color = ImVec4(0.5f, 0.5f, 0.5f, 1.0f);
+                            break;
                         }
-                        else if (i == 1)
-                        {
-                            float column_width = ImGui::GetColumnWidth();
-                            float text_width = ImGui::CalcTextSize(log->m_filter.c_str()).x;
-                            ImGui::TextColored(ImVec4(1.0f, 1.0f, 1.0f, 0.5f), log->m_filter.c_str());
-                        }
-                        else if (i == 2)
-                        {
-                            float column_width = ImGui::GetColumnWidth();
-                            float text_width = ImGui::CalcTextSize(log->m_timestamp.c_str()).x;
-                            ImGui::TextColored(ImVec4(1.0f, 1.0f, 1.0f, 0.5f), log->m_timestamp.c_str());
-                        }
-                        else if (i == 3)
-                        {
-                            ImVec4 message_color;
-                            if (log->m_level == spdlog::level::critical)
-                            {
-                                message_color = ImVec4(0.5f, 0.0f, 0.0f, 1.0f);
-                            }
-                            else if (log->m_level == spdlog::level::err)
-                            {
-                                message_color = ImVec4(0.8f, 0.2f, 0.2f, 1.0f);
-                            }
-                            else if (log->m_level == spdlog::level::warn)
-                            {
-                                message_color = ImVec4(1.0f, 1.0f, 0.0f, 1.0f);
-                            }
-                            else
-                            {
-                                message_color = ImVec4(1.0f, 1.0f, 1.0f, 0.8f);
-                            }
 
-                            ImGui::PushTextWrapPos(ImGui::GetCursorPosX() + ImGui::GetColumnWidth());
-                            ImGui::TextColored(message_color, log->m_message.c_str());
-                            ImGui::PopTextWrapPos();
+                        ImVec2 pos = ImGui::GetCursorScreenPos();
+                        float width = ImGui::GetFontSize() * 0.4f;
+                        float height = ImGui::GetFontSize() * 1.2f;
+                        float rounding = 3.0f;
+                        ImVec2 rect_min = pos;
+                        ImVec2 rect_max = ImVec2(pos.x + width, pos.y + height);
+
+                        ImGui::GetWindowDrawList()->AddRectFilled(rect_min, rect_max, ImGui::GetColorU32(color), rounding);
+
+                        switch (log->m_level)
+                        {
+                        case spdlog::level::critical:
+                            ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 10.0f);
+                            ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 2.0f);
+                            ImGui::Text("FATAL");
+                            break;
+                        case spdlog::level::err:
+                            ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 10.0f);
+                            ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 2.0f);
+                            ImGui::Text("Error");
+                            break;
+                        case spdlog::level::warn:
+                            ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 10.0f);
+                            ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 2.0f);
+                            ImGui::Text("Warning");
+                            break;
+                        case spdlog::level::info:
+                            ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 4.0f);
+                            ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 2.0f);
+                            ImGui::Text("Info");
+                            break;
+                        default:
+                            ImGui::Text("");
+                            break;
                         }
                     }
-
-                    if (is_hovered)
+                    else if (i == 1)
                     {
-                        ImGui::PopStyleColor();
+                        float column_width = ImGui::GetColumnWidth();
+                        float text_width = ImGui::CalcTextSize(log->m_filter.c_str()).x;
+                        ImGui::TextColored(ImVec4(1.0f, 1.0f, 1.0f, 0.5f), log->m_filter.c_str());
+                    }
+                    else if (i == 2)
+                    {
+                        float column_width = ImGui::GetColumnWidth();
+                        float text_width = ImGui::CalcTextSize(log->m_timestamp.c_str()).x;
+                        ImGui::TextColored(ImVec4(1.0f, 1.0f, 1.0f, 0.5f), log->m_timestamp.c_str());
+                    }
+                    else if (i == 3)
+                    {
+                        ImVec4 message_color;
+                        if (log->m_level == spdlog::level::critical)
+                        {
+                            message_color = ImVec4(0.5f, 0.0f, 0.0f, 1.0f);
+                        }
+                        else if (log->m_level == spdlog::level::err)
+                        {
+                            message_color = ImVec4(0.8f, 0.2f, 0.2f, 1.0f);
+                        }
+                        else if (log->m_level == spdlog::level::warn)
+                        {
+                            message_color = ImVec4(1.0f, 1.0f, 0.0f, 1.0f);
+                        }
+                        else
+                        {
+                            message_color = ImVec4(1.0f, 1.0f, 1.0f, 0.8f);
+                        }
+
+                        ImGui::PushTextWrapPos(ImGui::GetCursorPosX() + ImGui::GetColumnWidth());
+                        ImGui::TextColored(message_color, log->m_message.c_str());
+                        ImGui::PopTextWrapPos();
                     }
                 }
 
-                ImGui::EndTable();
+                if (is_hovered)
+                {
+                    ImGui::PopStyleColor();
+                }
             }
+
+            ImGui::EndTable();
         }
-        ImGui::EndChild();
+
         ImGui::PopStyleColor();
     }
 
