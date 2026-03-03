18a847169bddb3c36bba325a5f1e77c440cc78b6
diegomrno <d@infinite.si>
2026-03-03 17:55:44 +0100

    update: Cherry

--- vxgui/editor/app/instances/doc_viewer/doc_viewer.cpp
+++ vxgui/editor/app/instances/doc_viewer/doc_viewer.cpp
@@ -49,6 +49,26 @@ void DocViewerAppWindow::SetupRenderCallback() {
   });
 }
 
-void DocViewerAppWindow::Render() { CherryGUI::Text("Hello doc"); }
+void DocViewerAppWindow::Render() {
+  std::string markdownText = R"(
+#H1 Header : Text and Links
+You can add [links like this one to enkisoftware](https://www.enkisoftware.com/)
+and lines will wrap well. You can also insert images ![image alt text](image
+identifier e.g. filename) Horizontal rules:
+***
+___
+*Emphasis* and **strong emphasis** change the appearance of the text.
+## H2 Header: indented text.
+  This text has an indent (two leading spaces).
+    This one has two.
+### H3 Header: Lists
+  * Unordered lists
+    * Lists can be indented with two extra spaces.
+  * Lists can have [links like this one to Avoyd](https://www.avoyd.com/) and
+*emphasized text*
+)";
+
+  ImGui::MarkdownView(markdownText);
+}
 
 } // namespace VortexEditor
